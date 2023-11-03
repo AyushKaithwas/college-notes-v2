b34f48b20359005696952a2832da2616bb94a2a3
Ayush Kaithwas <ayush.91011@gmail.com>
2023-11-04 01:21:31 +0530

    the microservice is fast enough to handle await for response

--- apps/web/app/api/(Notes)/upload-note/route.ts
+++ apps/web/app/api/(Notes)/upload-note/route.ts
@@ -4,30 +4,6 @@ import { type NextRequest, NextResponse } from "next/server";
 import axios, { type AxiosResponse } from "axios";
 import prisma from "@/lib/prisma";
 
-// interface Payload {
-//   title: string;
-// }
-
-// export async function POST(): Promise<NextResponse> {
-//   // const res = (await req.json()) as Payload;
-//   const data = {
-//     title: "title1",
-//     userId: 1,
-//     notesLink: "https://www.google.com/",
-//   };
-//   await prisma.note
-//     .create({
-//       data,
-//     })
-//     .then((note) => {
-//       return NextResponse.json(note.id);
-//       // return res.status(201).send(document[0].id);
-//     })
-//     .catch((err) => {
-//       throw err;
-//     });
-//   return NextResponse.json(data, { status: 200 });
-
 interface Payload {
   title: string;
   description: string;
@@ -75,47 +51,28 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
       data: noteDataDb,
     })
     .then(async (note) => {
-      console.log("note", note);
-      console.log(process.env.PDF_TO_IMG_URL);
       if (process.env.PDF_TO_IMG_URL) {
-        console.log("Entered if block");
-        // Use the function:
         await makeRequest<ApiResponse>(
           process.env.PDF_TO_IMG_URL,
-          noteDataDb.notesLink,
-          1
+          noteDataDb.notesLink
         )
           .then((res3) => {
-            console.log(res3?.data.url);
+            prisma.note
+              .update({
+                where: {
+                  id: note.id,
+                },
+                data: {
+                  thumbnail: res3?.data.url,
+                },
+              })
+              .catch((err) => {
+                console.log(err);
+              });
           })
           .catch((err) => {
             console.log(err);
           });
-        // // .then((res3) => {
-        // //   console.log("entered ");
-        // //   console.log("res3", res3);
-        // //   if (res3?.data.url) {
-        // //     const thumbnail = res3.data.url;
-        // //     console.log("thumbnail", thumbnail);
-        // //     prisma.note
-        // //       .update({
-        // //         where: {
-        // //           id: note.id,
-        // //         },
-        // //         data: {
-        // //           thumbnail,
-        // //         },
-        // //       })
-        // //       .catch((err) => {
-        // //         console.log(err);
-        // //       });
-        // //   }
-        // //   console.log("Success");
-        // // })
-        // .catch((err) => {
-        //   console.log(err);
-        //   console.log("Reached max retries. Giving up.");
-        // });
       }
       return NextResponse.json(note.id, { status: 200 });
     })
@@ -129,21 +86,13 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
   return NextResponse.json({ Response: "Recieved" }, { status: 200 });
 }
 
-const MAX_RETRIES = 5;
-
 const makeRequest = async <T>(
   url: string,
-  pdfUrl: string,
-  attempt = 1
+  pdfUrl: string
 ): Promise<AxiosResponse<T> | undefined> => {
   try {
-    console.log("Entered makeRequest");
     return await axios.post<T>(url, { pdfUrl });
   } catch (err) {
-    if (attempt < MAX_RETRIES) {
-      console.log(`Attempt ${attempt} failed. Retrying...`);
-      return makeRequest(url, pdfUrl, attempt + 1);
-    }
     console.log(err);
     return undefined;
   }
